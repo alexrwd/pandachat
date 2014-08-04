192c0b6f6e0bf7dd4895be5fc38a805e478fcf7c
alex_rwd <alex.rwd1@gmail.com>
2014-08-04 10:22:10 +0300

    Initial security configuration.

--- src/main/java/com/pandachat/video/controller/ChatController.java
+++ src/main/java/com/pandachat/video/controller/ChatController.java
@@ -2,7 +2,9 @@ package com.pandachat.video.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Alex on 7/29/14.
@@ -14,5 +16,10 @@ public class ChatController {
     @RequestMapping("/chat")
     public String helloWorld(Model model) {
         return "chat";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(ModelMap model) {
+        return "login";
     }
 }
